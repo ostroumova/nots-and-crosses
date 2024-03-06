14b74781f801e6eb7e68fdca07040e3ac10be8c0
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-03-06 17:00:40 +0000

    adding tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
 import App from "./App.tsx";
 
 describe("App", () => {
   test("renders App component", () => {
     // Arrange
     render(<App />);
+    screen.debug();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // setupFiles: ["./test/setupTests.ts"],
+    coverage: {
+      include: ["src/**"],
+      extension: [".ts", ".tsx"],
+      reporter: ["text", "html", "json-summary"],
+      provider: "v8",
+      all: true,
+      // exclude: [
+      //   ...(configDefaults.coverage.exclude ?? []),
+      //   "**/types/**",
+      //   "**/types.ts",
+      // ],
+    },
+  },
+});
