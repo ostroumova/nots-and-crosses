e6822194d04a6c1491bbdce6a2004d833a40aa80
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-03-13 16:06:26 +0000

    map

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import "./styles.scss";
 
 const Board: React.FC = () => {
+  const squares = new Array(9).fill(null);
+
   return (
-    <>
-      <div className="board">
-        <div className="board__row">
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-        </div>
-        <div className="board__row">
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-        </div>
-        <div className="board__row">
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-          <button className="board__square"></button>
-        </div>
-      </div>
-    </>
+    <div className="board">
+      {squares.map((_, index) => {
+        const isRowStart = index % 3 === 0;
+        return (
+          <React.Fragment key={index}>
+            {isRowStart && <div className="board__row"></div>}
+            <button className="board__square"></button>
+          </React.Fragment>
+        );
+      })}
+    </div>
   );
 };
 
