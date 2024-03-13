624ccfe3ec5d7f7306fd7c325bca38356d5148bf
Galina Ostroumova <galina.ostroumova@ecs.co.uk>
2024-03-13 18:29:12 +0000

    add renderSquares

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./styles.scss";
 
 const Board: React.FC = () => {
-  const squares = new Array(9).fill(null);
+  const renderSquares = () => {
+    const squares = [];
+    for (let i = 0; i < 3; i++) {
+      squares.push(<button key={i} className="board__square"></button>);
+    }
+    return squares;
+  };
 
   return (
-    <div className="board">
-      {squares.map((_, index) => {
-        const isRowStart = index % 3 === 0;
-        return (
-          <React.Fragment key={index}>
-            {isRowStart && <div className="board__row"></div>}
-            <button className="board__square"></button>
-          </React.Fragment>
-        );
-      })}
-    </div>
+    <>
+      <div className="board">
+        <div className="board__row">{renderSquares()}</div>
+        <div className="board__row">{renderSquares()}</div>
+        <div className="board__row">{renderSquares()}</div>
+      </div>
+    </>
   );
 };
 
